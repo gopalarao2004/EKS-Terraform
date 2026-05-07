32f769a5f674436cb61f7ca5e7f703350de67bf0
Goopalarao <yadlapalligopalaraob4@gmail.com>
2026-05-07 12:04:54 +0530

    Change S3 bucket name in provider.tf
    Updated S3 bucket name for Terraform backend configuration.

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "eks-tf-reya-bkt"
+    bucket         = "eks-tf-statefile-b4"
     key            = "aws-eks-terraform.tfstate"
     region         = "ap-south-1"
     # dynamodb_table = "terraform_state"
